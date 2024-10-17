8ed05c1916a132fad7c12b983704a2b496ab9412
Viet Duc Kieu <viet.duc.kieu@web.de>
2024-10-17 10:45:40 +0200

    adjust root path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { CalendarDateRangePicker } from "@/components/dashboard/date-range-picker";
 
 export default function Home() {
   return (
     <div>
       <main>
         <p>Hello World</p>
+        <CalendarDateRangePicker />
       </main>
     </div>
   );
